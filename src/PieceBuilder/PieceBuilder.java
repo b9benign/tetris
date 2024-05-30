3c44c44cb94e9a1057ed7a163caafacb64273c3e
b9benign <100318396+b9benign@users.noreply.github.com>
2024-05-30 23:25:00 +0200

    documented PieceOption and PieceBuilder

--- src/PieceBuilder/PieceBuilder.java
+++ src/PieceBuilder/PieceBuilder.java
@@ -8,6 +8,23 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+/**
+ * This class renders a five by five field of clickable <code>PieceOption</code> buttons. It serves as a simple drawing board
+ * for creating custom Tetris pieces. <code>PieceBuilder</code> provides automated target validation, click-event handling,
+ * and helper functions that transform its current selection of <code>PieceOption</code> into a valid
+ * and optimized instance of <code>Figure</code> - a Tetris piece.
+ * <p>
+ * It is intended to be used within a <code>CustomPiecesCreationPanel</code>.
+ * </p>
+ *
+ * @author Jannis
+ * @version 1.0
+ * @see PieceOption
+ * @see Figure
+ * @see JPanel
+ * @see CustomPiecesCreationPanel
+ */
+
 public class PieceBuilder extends JPanel {
 
     private static final int PANEL_HEIGHT = 480;
@@ -19,24 +36,49 @@ public class PieceBuilder extends JPanel {
     private final Set<Integer> validTargets = new HashSet<>();
     private int availableOptionsAmount;
 
+    /**
+     * Constructs an instance of <code>PieceBuilder</code> that sets its default state by leveraging helper functions
+     * that clear any pre-existing state, set the preferred <code>JPanel</code> size, create the matrix/ field of <code>PieceOptions</code>
+     * and calculate valid targets.
+     *
+     * @see PieceOption
+     * @see JPanel
+     */
     public PieceBuilder() {
         clearCurrent();
         setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         setLayout(new GridLayout(BUILDER_SIZE, BUILDER_SIZE));
-
         create();
-        availableOptionsAmount = 13;
+        availableOptionsAmount = 9;
         fetchValidTargets();
     }
 
+    /**
+     * Returns the static width of this <code>PieceBuilder's</code> <code>JPanel</code>.
+     *
+     * @return the width of the JPanel.
+     * @see JPanel
+     */
     public static int getPanelWidth() {
         return PANEL_WIDTH;
     }
 
+    /**
+     * Returns the static height of this <code>PieceBuilder's</code> <code>JPanel</code>.
+     *
+     * @return the height of the JPanel.
+     * @see JPanel
+     */
     public static int getPanelHeight() {
         return PANEL_HEIGHT;
     }
 
+    /**
+     * Resets this <code>PieceBuilder's</code> state to its default. Resets the selection to the standard value, calculates valid
+     * targets anew and recreates the matrix/ field of <code>PieceOptions</code>.
+     *
+     * @see PieceOption
+     */
     public void clearCurrent() {
         selectedOptions.clear();
         selectedOptions.add(12);
@@ -45,6 +87,14 @@ public class PieceBuilder extends JPanel {
         create();
     }
 
+    /**
+     * Accesses <code>PieceBuilder's</code> current selection, cuts unused white space in the <code>PieceOption</code> matrix/ field
+     * and returns the smallest possible instance of <code>Figure</code> with a random color. This also resets the <code>PieceBuilder</code>
+     * state to instantly enable new creations.
+     *
+     * @return <code>Figure</code>
+     * @see Figure
+     */
     public Figure createCustomPieceFromCurrentBuilder() {
 
         int adjustedSize = getLargestSideOfOptions();
@@ -57,32 +107,32 @@ public class PieceBuilder extends JPanel {
 
     private void create() {
         removeAll();
-        for (int i = 0; i < BUILDER_SIZE * BUILDER_SIZE; i++) {
+        for (int i = 0; i < BUILDER_SIZE * BUILDER_SIZE; i++) {                         //create a 5x5 field of PieceOptions
             PieceOption pieceOption = new PieceOption(i);
             pieceOption.addActionListener(e -> {
                 PieceOption pieceOption1 = (PieceOption) e.getSource();
-                handleOptionClick(pieceOption1);
+                handleOptionClick(pieceOption1);                                        //handle click events
             });
             pieceOption.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
             options[i] = pieceOption;
-            add(pieceOption);
+            add(pieceOption);                                                           //add PieceOption to view
         }
-        updatePieceOptionsFromPieceBuilderState();
+        updatePieceOptionsFromPieceBuilderState();                                      //initial coloring
         revalidate();
         repaint();
     }
 
     private void handleOptionClick(PieceOption pieceOption) {
-
         int identifier = pieceOption.getIdentifier();
         if(selectedOptions.contains(identifier) || !validTargets.contains(identifier)) return;
         selectedOptions.add(identifier);
+
         fetchValidTargets();
         updatePieceOptionsFromPieceBuilderState();
     }
 
     private void updatePieceOptionsFromPieceBuilderState() {
-        for (PieceOption pieceOption : options) {
+        for (PieceOption pieceOption : options) {                                       //fetch association with sets for every PieceOption -> color accordingly
             int identifier = pieceOption.getIdentifier();
             boolean isSelected = selectedOptions.contains(identifier);
             boolean isValid = validTargets.contains(identifier);
@@ -103,6 +153,8 @@ public class PieceBuilder extends JPanel {
     private void fetchValidTargets() {
         if(!validTargets.isEmpty()) validTargets.clear();
         if(availableOptionsAmount <= 0) return;
+
+        //calculate neighboring indices of all selected options
         for(int selectedOption : selectedOptions) {
             int[] neighbors = {
                     selectedOption - BUILDER_SIZE,
@@ -110,6 +162,8 @@ public class PieceBuilder extends JPanel {
                     selectedOption - 1,
                     selectedOption + 1,
             };
+
+            //check neighbors eligibility for each selected option -> add to validTargets
             for(int neighbor : neighbors) {
                 if(selectedOptions.contains(neighbor) || neighbor < 0 || neighbor > (BUILDER_SIZE*BUILDER_SIZE)) continue;
                 int row = selectedOption / BUILDER_SIZE;
@@ -128,6 +182,7 @@ public class PieceBuilder extends JPanel {
         int bottom = 0;
         int right = 0;
 
+        //find indices of outermost selections
         for(int i = 0; i < BUILDER_SIZE; i++) {
             for(int j = 0; j < BUILDER_SIZE; j++) {
                 if(selectedOptions.contains(options[i * BUILDER_SIZE + j].getIdentifier())) {
@@ -138,11 +193,24 @@ public class PieceBuilder extends JPanel {
                 }
             }
         }
+
+        //return the bigger value out of width and height
         return Math.max((right - left + 1), (bottom - top + 1));
     }
 
     private PieceOption[] minimizeOptionsWhitespace(int newSize) {
         if (newSize == BUILDER_SIZE) return options;
+
+        /*
+            if the selection can be represented by a square with a size < BUILDER_SIZE (5), resize selection to
+            smaller square with a size of 5 > newSize > 0, e.g.:
+
+                [ ][X][X][ ][ ]      ->      [X][X][ ]
+                [ ][ ][X][X][ ]              [ ][X][X]
+                [ ][X][X][ ][ ]              [X][X][ ]
+                [ ][ ][ ][ ][ ]
+                [ ][ ][ ][ ][ ]
+         */
 
         int top = BUILDER_SIZE;
         int left = BUILDER_SIZE;
@@ -156,6 +224,7 @@ public class PieceBuilder extends JPanel {
                 }
             }
         }
+
         for (int i = top; i < top + newSize; i++) {
             for (int j = left; j < left + newSize; j++) {
                 if (i < BUILDER_SIZE && j < BUILDER_SIZE) {
@@ -165,12 +234,14 @@ public class PieceBuilder extends JPanel {
                 }
             }
         }
+
         return minimizedOptions;
     }
 
     private int[] mapOptionsToIndices(PieceOption[] minimizedOptions) {
         List<Integer> visibleBlocks = new ArrayList<>();
 
+        //map set of selections to indices usable for Figure-creation
         for(PieceOption pieceOption : minimizedOptions) {
             if (selectedOptions.contains(pieceOption.getIdentifier())) {
                 for (int i = 0; i < minimizedOptions.length; i++) {
@@ -178,11 +249,12 @@ public class PieceBuilder extends JPanel {
                 }
             }
         }
+
         return visibleBlocks.stream().mapToInt(i -> i).toArray();
     }
 
     private Color fetchRandomColor() {
-        Color[] colors = { Color.RED, Color.BLUE, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.YELLOW, Color.PINK, Color.BLACK };
+        Color[] colors = { Color.RED, Color.BLUE, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.YELLOW, Color.PINK, Color.ORANGE };
         Random random = new Random();
         int randomIndex = random.nextInt(colors.length);
         return colors[randomIndex];
